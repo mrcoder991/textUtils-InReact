c2a4307ffe0ad0aa20dc026eea084d59313eaeeb
mrcoder991 <udaygirhepunje41@gmail.com>
2022-03-07 17:24:24 +0530

    added more functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,21 +14,48 @@ export default function TextForm(props) {
 
   const handleUpChange = (event) => {
     setText(event.target.value);
-    
+    setCopy("Copy to clipboard")
+  };
+
+  const handleClrClick = () => {
+    setText("")
+  };
+
+  const handleCopyToClip = () => {
+    var text = document.getElementById("myBox")
+    text.select();
+    text.setSelectionRange(0, 9999);
+    navigator.clipboard.writeText(text.value)
+      .then(() => {
+        setCopy("Copied to clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
   };
 
   const [text, setText] = useState("");
   // text = "new text"; // wrong way to update
   // setText("new text"); // correct way to update
+
+  const [copy, setCopy] = useState("Copy to clipboard")
   return (
     <>
       <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
-          <textarea className="form-control" value={text} onChange={handleUpChange} placeholder="Enter text here" id="exampleFormControlTextarea1" rows="8"></textarea>
+          <textarea className="form-control" value={text} onChange={handleUpChange} placeholder="Enter text here" id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary m-2" onClick={handleUpClick}>Convert to UPPERCASE</button>
         <button className="btn btn-primary m-2" onClick={handleLoClick}>Convert to lowercase</button>
+        <button className="btn btn-primary m-2" onClick={handleExtraSpaces}>Remove extra spaces</button>
+        <button className="btn btn-success m-2" onClick={handleCopyToClip}>{copy}</button>
+        <button className="btn btn-danger m-2" onClick={handleClrClick}>Clear text</button>
       </div>
       <div className="container my-3">
         <h2>Your text summery</h2>
